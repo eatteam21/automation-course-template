39328ae5541b8e1ce25661bfaa12a52f2fc36f96
Lan Nguyen <lannguyen180796@email.com>
2024-04-21 19:55:26 +0700

    new

--- src/main/java/com/utils/BasicTest.java
+++ src/main/java/com/utils/BasicTest.java
@@ -11,24 +11,51 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 
 
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+
+
+
 public abstract class BasicTest {
     
     public static final Logger logger = LogManager.getLogger();
-    protected static WebDriver driver;
+    protected static AndroidDriver  driver;
+    // protected static WebDriver driver;
     // private String driverPath;
 
     @BeforeMethod
     public void preCondition() {
-        // Chromedriver path
-        // driverPath = "src/main/resources/WebDrivers/chromedriver.exe";
-        // ChromeOptions options = new ChromeOptions();
-        // System.setProperty("webdriver.chrome.driver", driverPath);
-        // driver = new ChromeDriver(options);
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        // Maximize the browser
-        driver.manage().window().maximize();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // // Chromedriver path
+        // // driverPath = "src/main/resources/WebDrivers/chromedriver.exe";
+        // // ChromeOptions options = new ChromeOptions();
+        // // System.setProperty("webdriver.chrome.driver", driverPath);
+        // // driver = new ChromeDriver(options);
+        // WebDriverManager.chromedriver().setup();
+        // driver = new ChromeDriver();
+        // // Maximize the browser
+        // driver.manage().window().maximize();
+        // //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        // capabilities.setCapability("platformVersion", "8.1");
+        // capabilities.setCapability("deviceName", "Pixel_2_XL_API_27");
+        //capabilities.setCapability("app", appLocation);
+        // //capabilities.setCapability("appPackage", "io.appium.android.apis");
+        // //capabilities.setCapability("appActivity", ".view.TextFields");
+        // capabilities.setCapability("appPackage", "com.swaglabsmobileapp");
+        // capabilities.setCapability("appActivity", "com.swaglabsmobileapp.SplashActivity");
+        // // capabilities.setCapability("appWaitActivity", "com.swaglabsmobileapp.MainActivity");
+        // // capabilities.setCapability("appWaitForLaunch", false);
+        capabilities.setCapability("automationName", "UiAutomator2");
+        // // capabilities.setCapability("udid", "emulator-5554");
+
+        try {
+            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        } catch (Exception e) {
+            // nothing
+        }
     }
 
     @AfterMethod
--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -3,17 +3,85 @@ package com;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.utils.Utils;
+
 import com.utils.BasicTest;
 
+
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
 public class LoginTest extends BasicTest {
+
+    private final String SAUCELABAPP_ACTIVITY = "com.swaglabsmobileapp.MainActivity";
+    private final String SAUCELABAPP_PACKAGE = "com.swaglabsmobileapp";
 
 
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://automationtestpro.com/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        // String url = "https://automationtestpro.com/";
+        // driver.get(url);
+        // Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        driver.startActivity(new Activity(SAUCELABAPP_PACKAGE, SAUCELABAPP_ACTIVITY));
+        Utils.hardWait(5000);
+
+
+        AndroidElement usernameBox = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'Username')]");
+        usernameBox.clear();
+        usernameBox.sendKeys("standard_user");
+
+        AndroidElement passwordBox = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'Password')]");
+        passwordBox.clear();
+        passwordBox.sendKeys("secret_sauce");
+
+        AndroidElement loginBtn = (AndroidElement) driver.findElementByXPath("//*[@content-desc='test-LOGIN']");
+        loginBtn.click();
+        Utils.hardWait();
+
+        AndroidElement notif = (AndroidElement) driver.findElementByXPath("(//*[@class='android.widget.TextView'])[3]");
+        Assert.assertTrue(notif.isDisplayed());
+
+//viet scrip flow add to cart - checkout - verify msg mua hang thanh cong
+//add to cart
+        AndroidElement addToCartBtn = (AndroidElement) driver.findElementByXPath("//*[@class='android.widget.TextView'])[1]");
+        loginBtn.click();
+        Utils.hardWait();
+//view cart
+        AndroidElement viewCartBtn = (AndroidElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc='test-Cart']/android.view.ViewGroup/android.widget.ImageView");
+        loginBtn.click();
+        Utils.hardWait();
+//checkout
+        AndroidElement checkOutBtn = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'CHECKOUT')]");
+        loginBtn.click();
+        Utils.hardWait();
+//input text
+        AndroidElement firtNameBox = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'First Name')]");
+        usernameBox.clear();
+        usernameBox.sendKeys("Lan");
+
+        AndroidElement lastNamedBox = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'Last Name')]");
+        passwordBox.clear();
+        passwordBox.sendKeys("Nguyen");
+
+        AndroidElement zipCodeBox = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'Zip/Postal Code')]");
+        passwordBox.clear();
+        passwordBox.sendKeys("1807");
+
+        AndroidElement continueBtn = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'CONTINUE')]");
+        loginBtn.click();
+        Utils.hardWait();
+//finish
+        AndroidElement finishBtn = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'FINISH')]");
+        loginBtn.click();
+        Utils.hardWait();
+//verify
+        AndroidElement checkoutSucc = (AndroidElement) driver.findElementByXPath("//*[contains(@text,'FINISH')]");
+        Assert.assertTrue(notif.isDisplayed());
+
+
     }
 
 }
