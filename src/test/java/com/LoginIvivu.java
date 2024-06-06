1024db8f80ccc09e40a396379b36db5cad6fd006
shiroelf <duongvan5801@gmail.com>
2024-06-06 21:32:47 +0700

    github test

--- src/test/java/com/LoginIvivu.java
+++ src/test/java/com/LoginIvivu.java
@@ -2,6 +2,7 @@ package com;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.pages.IvivuLoginPage;
@@ -9,8 +10,8 @@ import com.pages.LoginPage;
 import com.utils.BasicTest;
 import com.utils.Utils;
 public class LoginIvivu extends BasicTest {
-    @Test()
-    public void LoginSucessfully() throws Exception{
+    @Test(dataProvider = "test-data-ivivu")
+    public void LoginSucessfully(String name, String password) throws Exception{
         IvivuLoginPage ivivuloginpage = new IvivuLoginPage(driver);
         String url = "https://www.ivivu.com/";
         driver.get(url);
@@ -18,12 +19,19 @@ public class LoginIvivu extends BasicTest {
         
         ivivuloginpage.usernameClick();
         ivivuloginpage.firstLogin();
-        ivivuloginpage.enterEmail("tetakok687@lucvu.com");
-        ivivuloginpage.enterPassword("$GA@%dMh]U@td7-");
+        ivivuloginpage.enterEmail(name);
+        ivivuloginpage.enterPassword(password);
         ivivuloginpage.clickLogin();
         ////span[contains(text(),'tetakok687@lucvucom')]
         WebElement usernameCheck = ivivuloginpage.usernameCheck();
         Assert.assertTrue(usernameCheck.isDisplayed());
     }
-    
+
+    @DataProvider(name ="test-data-ivivu")
+    public Object[][] testData() {
+    Object [][] test_data_ivivu = new Object [1][2];
+    test_data_ivivu[0][0] = "tetakok687@lucvu.com";
+    test_data_ivivu[0][1] = "$GA@%dMh]U@td7-";
+    return test_data_ivivu;
+}
 }
