cf89876fceeafc5c658c5c8309a8d7b97f37fadb
minhphat3575 <minhphat3575@gmail.com>
2024-06-15 03:22:20 +0700

    BTVN

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -1,19 +1,54 @@
 package com;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.mongodb.util.Util;
 import com.utils.BasicTest;
+import com.utils.Utils;
 
 public class LoginTest extends BasicTest {
 
 
     @Test()
-    public void loginTest() throws Exception {
+    public void loginTestSuccess() throws Exception {
         // Launch website
-        String url = "https://google.com/";
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("testtest @gmail.com");
+        Utils.hardWait();
+
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Testtest@1");
+        Utils.hardWait();
+
+        driver.findElement(By.name("login")).click();
+        Utils.hardWait();
+
+        Assert.assertTrue(driver.findElement(By.name("login")).isDisplayed());
+    }
+
+    @Test()
+    public void loginTestFailed() throws Exception {
+        // Launch website
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("testtesta@gmail.com");
+        Utils.hardWait();
+
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Testtest@12");
+        Utils.hardWait();
+
+        driver.findElement(By.name("login")).click();
+        Utils.hardWait();
+
+        Assert.assertEquals("Địa chỉ email không xác . Kiểm tra lại hoặc thử tên người dùng của bạn.", "Địa chỉ email không xác định. Kiểm tra lại hoặc thử tên người dùng của bạn.");
     }
 
 }
