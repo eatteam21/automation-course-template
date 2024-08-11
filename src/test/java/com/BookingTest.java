b8275d01d542ed63f06c9f95c3536fb11793668b
ThanhDuy <hiiamduybui@gmail.com>
2024-08-11 10:23:42 +0700

    bai 2

---
+++ src/test/java/com/BookingTest.java
@@ -0,0 +1,60 @@
+package com;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.utils.BasicTest;
+import com.utils.Utils;
+
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+public class BookingTest extends BasicTest {
+    
+
+    private final String SAUCELABAPP_ACTIVITY = "com.swaglabsmobileapp.MainActivity";
+    private final String SAUCELABAPP_PACKAGE = "com.swaglabsmobileapp";
+
+    @Test()
+    public void loginTest() throws Exception {
+        // Launch app
+        driver.startActivity(new Activity(SAUCELABAPP_PACKAGE, SAUCELABAPP_ACTIVITY));
+        //Assert.assertEquals(driver.getCurrentUrl(), url);
+        Utils.hardWait(8000); // wait 5s
+
+        // locator ->  sendkey -> click login
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc='test-Username']")).sendKeys("standard_user");
+
+        // enter password
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]")).sendKeys("secret_sauce");
+        // click login
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]")).click();
+        Utils.hardWait(9000);
+        //add to cart
+        driver.findElement(By.xpath("(//android.view.ViewGroup[@content-desc=\"test-ADD TO CART\"])[1]")).click();
+        //go to cart
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-Cart\"]/android.view.ViewGroup/android.widget.ImageView")).click();
+        //checkout
+        driver.findElement(By.xpath("//android.widget.TextView[@text=\"CHECKOUT\"]")).click();
+        //FirstName
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-First Name\"]")).sendKeys("Thanh");
+        //LastName
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-Last Name\"]")).sendKeys("Duy");
+        //zip
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-Zip/Postal Code\"]")).sendKeys("848484");
+        //Continue
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-CONTINUE\"]")).click();
+        //Finish
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-FINISH\"]")).click();
+        //Backhome
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-BACK HOME\"]")).click();
+        //Wait
+
+        Utils.hardWait(10000); // 3s  
+
+
+
+
+    }
+
+}
--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -1,19 +1,72 @@
 package com;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
+import com.utils.Utils;
+
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
 
 public class LoginTest extends BasicTest {
+    
+
+    private final String SAUCELABAPP_ACTIVITY = "com.swaglabsmobileapp.MainActivity";
+    private final String SAUCELABAPP_PACKAGE = "com.swaglabsmobileapp";
+
+    //@Test()
+    public void loginTestSucess() throws Exception {
+        // Launch app
+        driver.startActivity(new Activity(SAUCELABAPP_PACKAGE, SAUCELABAPP_ACTIVITY));
+        //Assert.assertEquals(driver.getCurrentUrl(), url);
+        Utils.hardWait(8000); // wait 5s
+
+        // locator ->  sendkey -> click login
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc='test-Username']")).sendKeys("standard_user");
+
+        // enter password
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]")).sendKeys("secret_sauce");
+        // click login
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]")).click();
+        Utils.hardWait(9000);
+
+
+        // verify login
+
+        Utils.hardWait(10000); // 3s  
+
+
+
+
+    }
 
 
     @Test()
-    public void loginTest() throws Exception {
-        // Launch website
-        String url = "https://www.google.com/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+    public void loginTestFailedUser() throws Exception {
+        // Launch app
+        driver.startActivity(new Activity(SAUCELABAPP_PACKAGE, SAUCELABAPP_ACTIVITY));
+        //Assert.assertEquals(driver.getCurrentUrl(), url);
+        Utils.hardWait(8000); // wait 5s
+
+        // locator ->  sendkey -> click login
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc='test-Username']")).sendKeys("");
+
+        // enter password
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]")).sendKeys("secret_sauce");
+        // click login
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]")).click();
+        Utils.hardWait(9000);
+
+
+        // verify login
+
+        Utils.hardWait(10000); // 3s  
+
+
+
+
     }
 
 }
