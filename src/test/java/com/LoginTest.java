b9dad8e9ef0a3ad2f8077f55a1d6b1e1373b268d
TrucMaiLe <mai.le@automationtestpro.com>
2024-10-20 11:47:21 +0700

    test2

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -10,7 +10,7 @@ public class LoginTest extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://github.com/TrucMaiLe/TML-automation-course-template/tree/bai-15/src";
+        String url = "https://www.google.com";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
