697d4f0d70f34f3d3a8237980e45c9cadced01fc
TrucMaiLe <mai.le@automationtestpro.com>
2024-10-20 11:50:18 +0700

    test3

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -10,7 +10,7 @@ public class LoginTest extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://www.facebook.com/";
+        String url = "https://www.google.com";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
